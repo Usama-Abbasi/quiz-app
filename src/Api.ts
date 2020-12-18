d3a5afa7eab23fa8da241c513990dbadd6a77c49
usama999abbasi <60782977+usama999abbasi@users.noreply.github.com>
2020-12-18 19:55:19 +0500

    First Comment

---
+++ src/Api.ts
@@ -0,0 +1,27 @@
+import { queries } from '@testing-library/react';
+import {shuffleArray} from './utilis'
+export type Question={
+    category:string;
+    correct_answer:string;
+    difficulty:string;
+    incorrect_answers:string[];
+    question:string,
+    type:string;
+}
+export enum Difficulty{
+    EASY='easy',
+    MEDIUM='medium',
+    HARD='hard'
+}
+export type QuestionState=Question & {answers:string[]};
+export const fetchQuestions=async(amount:number,difficulty:Difficulty):Promise<QuestionState[]>=>{
+    console.log(amount,difficulty);
+    const url=`https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+    const data = await (await fetch(url)).json();
+    // console.log(data);
+    return data.results.map((question:Question)=>({
+        ...question,
+        answers:shuffleArray([...question.incorrect_answers,question.correct_answer])
+    }));
+
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import {fetchQuestions,Difficulty} from './Api';
+import { GlobalStyle, Wrapper } from './App.styles';
+import {QuestionState} from './Api';
+import QuestionCard from './components/QuestionCard';
+const TOTAL_QUESTION=10;
+export type AnswerObject={
+  question:string;
+  answer:string;
+  correct:boolean;
+  correctAnswer:string;
 
+}
 function App() {
+  const [isLoading, setisLoading] = useState(false);
+  const [questions, setquestions] = useState<QuestionState[]>([]);
+  const [number, setnumber] = useState(0);
+  const [useranswer, setuseranswer] = useState<AnswerObject[]>([]);
+  const [score, setscore] = useState(0);
+  const [gameOver, setgameOver] = useState(true);
+  const startQuiz=async()=>{
+    setisLoading(true);
+    setgameOver(false);
+    const questions=await fetchQuestions(TOTAL_QUESTION,Difficulty.MEDIUM);
+    console.log(questions);
+    setquestions(questions);
+    setnumber(0);
+    setuseranswer([]);
+    setscore(0);
+    setisLoading(false);
+   
+  }
+  
+  const checkAnswer=(e:any)=>{
+    if(!gameOver){
+      const userAnswer=e.target.value;
+      const correct=questions[number].correct_answer===userAnswer;
+      if(correct) setscore(prev=>prev+1);
+      const answerObject={
+        question:questions[number].question,
+        answer:userAnswer,
+        correct,
+        correctAnswer:questions[number].correct_answer
+      }
+      setuseranswer((prev)=>[...prev,answerObject]);
+    }
+
+  }
+  const nextQuestion=()=>{
+      const nextQuestion=number+1;
+      if(nextQuestion===TOTAL_QUESTION)
+      {
+        setgameOver(true);
+      }
+      else{
+        setnumber(nextQuestion);
+      }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <GlobalStyle/>
+    <Wrapper>
+      <h1>React Quiz</h1>
+      {gameOver|| useranswer.length===TOTAL_QUESTION?(<button onClick={startQuiz} className='start'>start</button>):null}
+      {!gameOver?<p className='score'>Score:{score}</p>:null}
+      {isLoading?<p>Loading...</p>:null}
+      {!isLoading && !gameOver &&(
+        <QuestionCard
+        questionNr={number+1}
+        totalQuestions={TOTAL_QUESTION}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={useranswer?useranswer[number]:undefined}
+        checkAnswer={checkAnswer}
+        />
+
+      )}
+        {!gameOver && !isLoading && useranswer.length === number + 1 && number !== TOTAL_QUESTION - 1 ? (
+          <button className='next' onClick={nextQuestion}>
+            Next Question
+          </button>
+        ) : null}
+    </Wrapper>
+    </>
   );
+ 
 }
 
 export default App;
